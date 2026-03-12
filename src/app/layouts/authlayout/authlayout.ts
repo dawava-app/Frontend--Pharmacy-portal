d5605a466377919cd756e6f9b5ed287a345f88c9
sondosEssam <sondosessam713@gmail.com>
2026-03-12 23:08:05 +0200

    add main flow

---
+++ src/app/layouts/authlayout/authlayout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authlayout',
+  imports: [],
+  templateUrl: './authlayout.html',
+  styleUrl: './authlayout.scss',
+})
+export class Authlayout {
+
+}
---
+++ src/app/layouts/mainlayout/mainlayout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainlayout',
+  imports: [],
+  templateUrl: './mainlayout.html',
+  styleUrl: './mainlayout.scss',
+})
+export class Mainlayout {
+
+}
